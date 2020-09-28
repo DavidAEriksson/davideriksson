c38fe79c685b9f0cab8348d25723dde685d70888
David Eriksson <daer3303@student.umu.se>
2020-09-28 20:33:17 +0200

    Added AOS

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,21 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
+import AOS from "aos";
 
 import "../styles/projects.scss";
+import "aos/dist/aos.css";
 
 const Projects = (props) => {
-  return (
-    <div className="project-wrapper" style={props.style}>
-      <h1>Projects</h1>
-      <p>Text text text text text text text text</p>
-      <div className="project-card-container">
-        <div className="project-card"></div>
-        <div className="project-card"></div>
-        <div className="project-card"></div>
-        <div className="project-card"></div>
-        <div className="project-card"></div>
-      </div>
-    </div>
-  );
+	useEffect(() => {
+		AOS.init({ duration: 1000 });
+	});
+	return (
+		<div className="project-wrapper" style={props.style}>
+			<h1>Projects</h1>
+			<p>Text text text text text text text text</p>
+			<div className="project-card-container">
+				<div data-aos="fade-right" className="project-card"></div>
+				<div data-aos="fade-left" className="project-card"></div>
+				<div data-aos="fade-right" className="project-card"></div>
+				<div data-aos="fade-left" className="project-card"></div>
+				<div data-aos="fade-right" className="project-card"></div>
+				<div data-aos="fade-left" className="project-card"></div>
+			</div>
+		</div>
+	);
 };
 
 export default Projects;
